42340ac4e9615e9be3c747304746450466990c7f
robinlovelace <rob00x@gmail.com>
2023-12-12 17:25:23 +0000

    Add starter on #30

---
+++ code/get-input-datasets.R
@@ -0,0 +1,12 @@
+# Aim: get input datasets for the paper
+
+rnet_wyca = pct::get_pct_rnet("west-yorkshire")
+# library(mapedit)
+
+# Visual inspection of OSM at 
+# https://www.openstreetmap.org/node/10438140#map=17/53.83244/-1.58852&layers=Y
+# And
+# https://www.openstreetmap.org/node/313102482#map=19/53.79826/-1.58538&layers=Y
+
+otley_road_point = c(53.8325274, -1.5910451)
+armley_road_point = c(53.7985648, -1.5852985)
