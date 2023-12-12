588fc18d9a106386b7488adbf9645e16f0b974fa
Robin Lovelace <Robinlovelace@users.noreply.github.com>
2023-12-12 23:47:02 +0000

    Add starter on #30 (#80)
    * Add starter on #30
    
    * Progress on #30
    
    * Tidy up, ht @josiahparry
    
    * Attempt to filter out largest group
    
    * Add more example data for #30
    
    * Update table and start on figure with input datasets
    
    * Update with plot and captions
    
    * Fix table caption
    
    * Caption tweaks

---
+++ code/get-input-datasets.R
@@ -0,0 +1,43 @@
+# Aim: get input datasets for the paper
+
+library(sf)
+rnet_wyca = pct::get_pct_rnet("west-yorkshire")
+# library(mapedit)
+
+# Visual inspection of OSM at 
+# https://www.openstreetmap.org/node/10438140#map=17/53.83244/-1.58852&layers=Y
+# And
+# https://www.openstreetmap.org/node/313102482#map=19/53.79826/-1.58538&layers=Y
+
+otley_road_point = c(-1.5910, 53.8325)
+armley_road_point = c(-1.5852, 53.7985)
+otley_road_point_sf = st_point(otley_road_point) |>
+  st_sfc(crs = 4326)
+armley_road_point_sf = st_point(armley_road_point) |>
+  st_sfc(crs = 4326)
+otley_road_buffer = st_buffer(otley_road_point_sf, 250)
+armley_road_buffer = st_buffer(armley_road_point_sf, 250)
+rnet_otley = rnet_wyca |>
+  st_intersection(otley_road_buffer) |>
+  st_cast("LINESTRING")
+rnet_armley = rnet_wyca |>
+  st_intersection(armley_road_buffer) |>
+  st_cast("LINESTRING")
+
+# keep only connected components
+# new function for stplanr?
+filter_connected = function(x, n_groups = 1){
+  g = stplanr::rnet_group(x, d = 1)
+  g_ordered = sort(table(g), decreasing = TRUE)
+  g_keep = names(g_ordered)[1:n_groups]
+  x = x[g %in% g_keep, ]
+  x
+}
+rnet_otley = filter_connected(rnet_otley)
+rnet_armley = filter_connected(rnet_armley)
+
+mapview::mapview(rnet_otley)
+mapview::mapview(rnet_armley)
+
+write_sf(rnet_otley, "data/rnet_otley.geojson")
+write_sf(rnet_armley, "data/rnet_armley.geojson")
